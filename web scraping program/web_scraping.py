91ad4fa3854505ab329f18c6c1871f85766db586
Param Dhama <dhamaparam@gmail.com>
2023-08-19 17:27:07 +0530

    Create web_scraping.py

---
+++ web scraping program/web_scraping.py
@@ -0,0 +1,57 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Send a GET request to the Flipkart search page
+uurl = "https://www.flipkart.com/search?sid=tyy%2C4io&p%5B%5D=facets.network_type%255B%255D%3D5G&ctx=eyJjYXJkQ29udGV4dCI6eyJhdHRyaWJ1dGVzIjp7InRpdGxlIjp7Im11bHRpVmFsdWVkQXR0cmlidXRlIjp7ImtleSI6InRpdGxlIiwiaW5mZXJlbmNlVHlwZSI6IlRJVExFIiwidmFsdWVzIjpbIlNob3AgTm93Il0sInZhbHVlVHlwZSI6Ik1VTFRJX1ZBTFVFRCJ9fX19fQ%3D%3D&otracker=clp_metro_expandable_1_5.metroExpandable.METRO_EXPANDABLE_Shop%2BNow_mobile-phones-store_92P8Y0U07S00_wp3&fm=neo%2Fmerchandising&iid=M_65cd8e01-08f1-4bdc-a05d-719ed0a8711e_5.92P8Y0U07S00&ppt=clp&ppn=mobile-phones-store&ssid=5enoi73xfk0000001685030864756&sort=price_asc&page="
+products = []
+
+
+
+pages = int(input("How many pages data u want\n"))
+urls = []
+
+for i in range(1,pages+1):
+    urls.append(uurl+str(i))
+
+for url in urls:
+    response = requests.get(url)
+    # Parse the HTML content
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # Find the product details on the page
+    product_elements = soup.find_all('div', {'class': '_2kHMtA'})
+    for product in product_elements:
+        name_element = product.find('div', {'class': '_4rR01T'})
+        name = name_element.text.strip() if name_element else "N/A"
+
+        price_element = product.find('div', {'class': '_30jeq3 _1_WHN1'})
+        price = price_element.text.strip() if price_element else "N/A"
+
+        review_element = product.find('div', {'class': '_3LWZlK'})
+        review = review_element.text.strip() if review_element else "N/A"
+
+        discription_element = product.find('ul', {'class': '_1xgFaf'})
+        discription_element_list = []
+        for dic in discription_element:
+            discription_element_list.append(dic.text.strip() if dic else "N/A")
+        discription = "/".join(discription_element_list)
+        imgscr_element = product.find('img', {'class': '_396cs4'})
+        img = imgscr_element['src']
+
+        a_element = product.find('a',{'class':'_1fQZEK'})
+        link = 'https://www.flipkart.com'+a_element['href']
+
+        products.append({
+            'Product Name': name,
+            'Price': price,
+            'Discription': discription,
+            'Image': img,
+            'Link': link
+        })
+
+# Convert the list of dictionaries to a DataFrame
+df = pd.DataFrame(products)
+
+
+# Save the DataFrame to a CSV file
+df.to_csv('C:\IMP\Job project\PowerBi\Mobile-Phones-Analysis/Data/flipkart_product.csv', index=False)
